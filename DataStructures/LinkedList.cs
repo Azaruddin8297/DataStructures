840a3276e362be6bf9975c27749e0e9879d31ad9
Azaruddin8297 <ajarpopo.com>
2023-03-28 22:37:02 +0530

    Simple

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = this.head;
+                if(temp.next != null)
+                {
+                   temp = temp.next;
+                }
+                temp = temp.next;
+            }
+            Console.WriteLine("{0} Inserted into Linked List", node.data);
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList link = new LinkedList();
+            link.Add(56);
+            link.Add(30);
+            link.Add(70);
+        }
+    }
+}
